127683ddb1be1ec6b80b45721b8dcd960be709fd
toan nguyen <avatar1341991@gmail.com>
2018-06-12 17:37:02 +0700

    temp

--- ToanShop/ToanShop.WebApp/wwwroot/app/shared/shared.js
+++ ToanShop/ToanShop.WebApp/wwwroot/app/shared/shared.js
@@ -3,7 +3,7 @@
         pageSize :10,
         pageIndex:1
     },
-    notify: function(massage,type){
+    notify: function(message,type){
         $.notify(message,{
               // whether to hide the notification on click
               clickToHide: true,
@@ -36,18 +36,122 @@
               gap: 2
         });
     },
-    confirm: function(massage,okCallback){
+    confirm: function (message, okCallback) {
+        bootbox.confirm({
+            size: "small",
+            message: message,
+            buttons: {
+                confirm: {
+                    label: 'Đồng ý',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'Huỷ',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                if (result === true) {
+                    okCallback();
+                }
+
+            }
+        });
     },
-    dateFormatJson : function(datetime){
+    dateFormatJson: function (datetime) {
+        if (datetime == null || datetime === '')
+            return '';
+        var newdate = new Date(datetime);
+        var month = newdate.getMonth() + 1;
+        var day = newdate.getDate();
+        var year = newdate.getFullYear();
+        var hh = newdate.getHours();
+        var mm = newdate.getMinutes();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        return day + "/" + month + "/" + year;
     },
-    dateTimeFormatJson : function(datetime){
+    dateTimeFormatJson: function (datetime) {
+        if (datetime === null || datetime === '')
+            return '';
+        var newdate = new Date(datetime);
+        var month = newdate.getMonth() + 1;
+        var day = newdate.getDate();
+        var year = newdate.getFullYear();
+        var hh = newdate.getHours();
+        var mm = newdate.getMinutes();
+        var ss = newdate.getSeconds();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        if (ss < 10)
+            ss = "0" + ss;
+        return day + "/" + month + "/" + year + " " + hh + ":" + mm + ":" + ss;
     },
-    startLoading:function(){
+    startLoading: function () {
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading').removeClass('hide');
     },
-    stopLoading:function(){
+    stopLoading: function () {
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading')
+                .addClass('hide');
     },
-    getStatus:function(status){
+    getStatus: function (status) {
+        if (status === 1)
+            return '<span class="badge bg-green">Kích hoạt</span>';
+        else
+            return '<span class="badge bg-red">Khoá</span>';
+
     },
-    formatNumber: function(number,precision){
+    formatNumber: function (number, precision) {
+        if (!isFinite(number)) {
+            return number.toString();
+        }
+
+        var a = number.toFixed(precision).split('.');
+        a[0] = a[0].replace(/\d(?=(\d{3})+$)/g, '$&,');
+        return a.join('.');
+    },
+    unflattern: function (arr) {
+        var map = {};
+        var roots = [];
+        for (var i = 0; i < arr.length; i += 1) {
+            var node = arr[i];
+            node.children = [];
+            map[node.Id] = i; // use map to look-up the parents
+            if (node.ParentId !== null) {
+                arr[map[node.ParentId]].children.push(node);
+            } else {
+                roots.push(node);
+            }
+        }
+        return roots;
+    },
+    unflattern1: function (arr) {
+        var map = {};
+        var roots = [];
+        for (var i = 0; i < arr.length; i += 1) {
+            var node = arr[i];
+            node.children = [];
+            map[node.id] = i; // use map to look-up the parents
+            if (node.parentId !== null) {
+                arr[map[node.parentId]].children.push(node);
+            } else {
+                roots.push(node);
+            }
+        }
+        return roots;
     }
 }
